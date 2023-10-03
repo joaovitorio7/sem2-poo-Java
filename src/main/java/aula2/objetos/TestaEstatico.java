c11ac49d333dbf4d62e92c98925fe81794062bae
Joao <joaoprodutor92@gmail.com>
2023-10-03 19:26:19 -0300

    explicação get and set

--- src/main/java/aula2/objetos/TestaEstatico.java
+++ src/main/java/aula2/objetos/TestaEstatico.java
@@ -2,8 +2,9 @@ package aula2.objetos;
 
 public class TestaEstatico {
     public static void main(String[] args) {
-        int totalContas = Conta.totalContas;
-        System.out.println(totalContas);
+        // int totalContas = Conta.totalContas;
+        System.out.println(Conta.totalContas);
+        System.out.println(Conta.getTotalContas());
 
         Conta conta1 = new Conta();
         System.out.println(conta1.getTotalContas());
